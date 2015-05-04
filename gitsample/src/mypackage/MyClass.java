6d1f8bc246490a7c793e87da6ea462deab5c1169
Hisao <Hisao@192.168.2.102>
2015-05-04 12:48:45 +0900

    mainメソッドに「Git Sample 3!」の表示を追加しました。

--- gitsample/src/mypackage/MyClass.java
+++ gitsample/src/mypackage/MyClass.java
@@ -5,6 +5,7 @@ public class MyClass {
 	public static void main(String[] args) {
 		System.out.println("Git Sample!");
 		System.out.println("Git Sample 2!");
+		System.out.println("Git Sample 3!");
 	}
 
 }
